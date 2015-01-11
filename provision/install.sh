f722fb9d2d55e5b10477ad72fcc228ef5617aa28
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-01-11 17:31:28 +0100

    add sublime text 2

--- provision/install.sh
+++ provision/install.sh
@@ -1,6 +1,7 @@
  #!/usr/bin/env bash
 
 #prerequisites
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
 sudo apt-get update
 sudo apt-get install -y curl
 
@@ -9,3 +10,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm use --install --default 2.1.5
+
+#sublime-text-2
+sudo apt-get install sublime-text
+cp /usr/share/applications/sublime-text-2.desktop /home/vagrant/Desktop/
