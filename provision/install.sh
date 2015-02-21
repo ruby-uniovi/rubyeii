b6bdf2408705483afa55d0d68009f2d2fe6d12b8
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-02-21 14:32:06 +0100

    remove useless sudos

--- provision/install.sh
+++ provision/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #prerequisites
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
-sudo apt-get update
-sudo apt-get install -y curl
+add-apt-repository ppa:webupd8team/sublime-text-2
+apt-get update
+apt-get install -y curl
 
 #rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
@@ -12,11 +12,11 @@ source /etc/profile.d/rvm.sh
 rvm use --install --default 2.1.5
 
 #sublime-text-2
-sudo apt-get install sublime-text
+apt-get install sublime-text
 cp /usr/share/applications/sublime-text-2.desktop /home/vagrant/Desktop/
 
 #mongodb
-sudo apt-get install -y mongodb
+apt-get install -y mongodb
 
 #heroku
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh 
