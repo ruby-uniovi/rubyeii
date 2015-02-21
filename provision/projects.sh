77e3382aceae9b1c6fc5890ef141fade817d89ae
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-02-21 14:43:42 +0100

    store projects in vagrant shared dir

--- provision/projects.sh
+++ provision/projects.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-mkdir -p projects
+BASE=/vagrant/projects
+mkdir -p "$BASE"
 for PROJECT in "jukebox-1" "jukebox-sinatra"; do
-  DEST="$HOME/projects/$PROJECT" 
+  DEST="$BASE/$PROJECT" 
   if [ -d "$DEST" ]; then
     cd $DEST && git pull
   else
-    cd "$HOME/projects" && git clone "https://github.com/ruby-uniovi/$PROJECT.git"
+    cd "$BASE" && git clone "https://github.com/ruby-uniovi/$PROJECT.git"
   fi 
 done
