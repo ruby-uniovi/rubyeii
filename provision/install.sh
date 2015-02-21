67a37be9de19bcf847cab104ed9a47d07c2e33ed
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-02-21 14:32:22 +0100

    allow vagrant user to install gems

--- provision/install.sh
+++ provision/install.sh
@@ -10,6 +10,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm use --install --default 2.1.5
+usermod -a -G rvm vagrant
 
 #sublime-text-2
 apt-get install sublime-text
