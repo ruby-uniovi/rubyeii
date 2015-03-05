cb4976c8fea90c37ec3cc2311f994ea8e8f651a6
Diego Guerra <diego.guerra.suarez@gmail.com>
2015-03-05 21:17:40 +0100

    try to solve gpg rvm problems

--- provision/install.sh
+++ provision/install.sh
@@ -7,6 +7,7 @@ apt-get install -y curl
 
 #rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+\curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 \curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm use --install --default 2.1.5
